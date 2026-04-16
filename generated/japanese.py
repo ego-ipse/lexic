42fb1c8ddc6b1aac8d312d2219dda4873662d740
Mika <teihenrique@gmail.com>
2026-04-16 20:21:01 +0100

    .

--- generated/japanese.py
+++ generated/japanese.py
@@ -1,4 +1,4 @@
-"""Auto-generated Pydantic models from /home/mika/projects/vyx_2/resources/ground_truth/japanese.gbnf."""
+"""Auto-generated Pydantic models from resources/ground_truth/japanese.gbnf."""
 from __future__ import annotations
 
 from abc import ABC
